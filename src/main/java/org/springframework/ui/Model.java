62f21fee6dc01327300f195986a2c91f936fa233
Vadym <trokhimchuk2003@gmail.com>
2023-08-20 08:06:36 +0300

    Added Code

---
+++ src/main/java/org/springframework/ui/Model.java
@@ -0,0 +1,19 @@
+package org.springframework.ui;
+
+import java.util.List;
+
+import com.example.model.Applicant;
+
+public class Model {
+
+	public void addAttribute(String string, List<Applicant> applicants) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addAttribute(String string, Applicant applicant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
